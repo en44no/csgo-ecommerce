418872687c3c702a5572fef4060952c531604275
NahuelMarquez <nahuel.marquez@gicla.com>
2023-04-05 18:00:06 -0300

    sound fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -125,10 +125,6 @@ export default function Home() {
     if (awpScopeAudio) {
       awpScopeAudio.volume = 0.1;
       awpScopeAudio.play();
-      setTimeout(() => {
-        awpScopeAudio.pause();
-        awpScopeAudio.currentTime = 0;
-      }, 1000);
     }
   };
 
@@ -140,6 +136,13 @@ export default function Home() {
         awpShootAudio.pause();
         awpShootAudio.currentTime = 0;
       }, 1000);
+    }
+  };
+
+  const pauseScopeSound = () => {
+    if (awpScopeAudio) {
+      awpScopeAudio.pause();
+      awpScopeAudio.currentTime = 0;
     }
   };
 
@@ -204,7 +207,7 @@ export default function Home() {
                   </Box>
 
                   {!skinsAreLoading && filteredSkins.map((skin, index) => (
-                    <Box onMouseOver={playScopeSound} onClick={() => onOpenModal(skin)} boxShadow='md' key={skin.Nombre + skin.Float + index} position='relative' bg='#1e2227' h={{ sm: 'auto', md: '10.5rem' }} minW={{ sm: '45%', md: '13rem' }} w={{ sm: '45%', md: '13rem' }} _hover={{ 'bg': '#3f3f45' }} cursor='pointer' borderRadius='9px'>
+                    <Box onMouseOver={playScopeSound} onMouseLeave={pauseScopeSound} onClick={() => onOpenModal(skin)} boxShadow='md' key={skin.Nombre + skin.Float + index} position='relative' bg='#1e2227' h={{ sm: 'auto', md: '10.5rem' }} minW={{ sm: '45%', md: '13rem' }} w={{ sm: '45%', md: '13rem' }} _hover={{ 'bg': '#3f3f45' }} cursor='pointer' borderRadius='9px'>
                       <Box className={isMobile ? 'skin-image-container' : 'scale-image'} position='relative' display='flex' flexDir='column' alignItems='center' gap={2} py={3} px={1}>
                         <Box h={{ sm: '6.5rem', md: '5.5rem' }} mt={{ sm: '-2rem', md: '-0.7rem' }} p={{ sm: 6, md: 0 }}>
                           <Image layout='responsive' className="shadow-for-skin-image" alt={skin.Nombre} width={{ sm: 'auto', md: '8.3rem' }} height='auto' style={{ 'objectFit': "cover" }} src={skin.ImagenURL}></Image>
